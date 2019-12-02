ccde3e01324208583fa3fb1084209872cf5d68d0
David Andersson <anderssonpublic@gmail.com>
2019-12-01 21:08:16 -0800

    Add function that checks whether foreign key is required based on artifacts

--- tests/open_alchemy/column_factory/test_object_ref.py
+++ tests/open_alchemy/column_factory/test_object_ref.py
@@ -709,6 +709,134 @@ def test_check_foreign_key_required_spec(model_schema, schemas, expected_require
 
 
 @pytest.mark.parametrize(
+    "model_schema",
+    [
+        {
+            "properties": {
+                "ref_table_fk_column": {"x-foreign-key": "ref_table.fk_column"}
+            }
+        },
+        {
+            "properties": {
+                "ref_table_fk_column": {
+                    "type": "not_fk_type",
+                    "x-foreign-key": "ref_table.fk_column",
+                }
+            }
+        },
+        {"properties": {"ref_table_fk_column": {"type": "fk_type"}}},
+        {
+            "properties": {
+                "ref_table_fk_column": {
+                    "type": "fk_type",
+                    "x-foreign-key": "wrong_table.wrong_column",
+                }
+            }
+        },
+    ],
+    ids=["no type", "wrong type", "no x-foreign-key", "wrong x-foreign-key"],
+)
+@pytest.mark.column
+@pytest.mark.object_ref
+def test_check_foreign_key_required_invalid_schema(model_schema):
+    """
+    GIVEN model schema that is not valid
+    WHEN check_foreign_key_required is called
+    THEN MalformedRelationshipError is raised.
+    """
+    artifacts = types.ObjectArtifacts(
+        "RefSchema",
+        fk_column_name="fk_column",
+        fk_column_artifacts=types.ColumnArtifacts(
+            "fk_type", foreign_key="ref_table.fk_column"
+        ),
+    )
+    fk_logical_name = "ref_table_fk_column"
+
+    with pytest.raises(exceptions.MalformedRelationshipError):
+        object_ref.check_foreign_key_required(
+            artifacts=artifacts,
+            fk_logical_name=fk_logical_name,
+            model_schema=model_schema,
+            schemas={},
+        )
+
+
+@pytest.mark.parametrize(
+    "model_schema, schemas, expected_required",
+    [
+        ({"properties": {}}, {}, True),
+        (
+            {
+                "properties": {
+                    "ref_table_fk_column": {
+                        "type": "fk_type",
+                        "x-foreign-key": "ref_table.fk_column",
+                    }
+                }
+            },
+            {},
+            False,
+        ),
+        (
+            {
+                "properties": {
+                    "ref_table_fk_column": {"$ref": "#/components/schemas/FkSchema"}
+                }
+            },
+            {"FkSchema": {"type": "fk_type", "x-foreign-key": "ref_table.fk_column"}},
+            False,
+        ),
+        (
+            {
+                "properties": {
+                    "ref_table_fk_column": {
+                        "allOf": [
+                            {"type": "fk_type", "x-foreign-key": "ref_table.fk_column"}
+                        ]
+                    }
+                }
+            },
+            {},
+            False,
+        ),
+    ],
+    ids=[
+        "not in model schema",
+        "in model schema",
+        "in model schema $ref",
+        "in model schema allOf",
+    ],
+)
+@pytest.mark.column
+@pytest.mark.object_ref
+def test_check_foreign_key_required(model_schema, schemas, expected_required):
+    """
+    GIVEN foreign key spec, foreign key logical name, model schema, schemas and
+        expected required
+    WHEN check_foreign_key_required is called
+    THEN the expected required is returned.
+    """
+    artifacts = types.ObjectArtifacts(
+        "RefSchema",
+        fk_column_name="fk_column",
+        fk_column_artifacts=types.ColumnArtifacts(
+            "fk_type", foreign_key="ref_table.fk_column"
+        ),
+    )
+    fk_logical_name = "ref_table_fk_column"
+
+    required = object_ref.check_foreign_key_required(
+        artifacts=artifacts,
+        fk_logical_name=fk_logical_name,
+        model_schema=model_schema,
+        schemas=schemas,
+    )
+
+    assert required == expected_required
+
+
+@pytest.mark.parametrize(
     "kwargs",
     [{"backref": None, "uselist": True}, {"secondary": "association"}],
     ids=["backref None uselist not None", "secondary not None"],
